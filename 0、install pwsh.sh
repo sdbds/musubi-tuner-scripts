a600ca134ed42bf6e7de6b24b29be44f74ee4d67
sdbds <865105819@qq.com>
2025-06-05 17:15:41 +0800

    update 2.7.1 for torch

--- 0、install pwsh.sh
+++ 0、install pwsh.sh
@@ -8,19 +8,19 @@ then
     echo "PowerShell 未安装，正在安装..."
     
     # 下载 PowerShell '.tar.gz' 压缩包
-    curl -L -o /tmp/powershell.tar.gz https://mirror.ghproxy.com/https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/powershell-7.4.6-linux-x64.tar.gz
+    curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.5.1/powershell-7.5.1-linux-x64.tar.gz
     
     # 创建目标文件夹
-    sudo mkdir -p /opt/microsoft/powershell/7
+    mkdir -p /opt/microsoft/powershell/7
     
     # 解压 PowerShell 到目标文件夹
-    sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+    tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
     
     # 设置执行权限
-    sudo chmod +x /opt/microsoft/powershell/7/pwsh
+    chmod +x /opt/microsoft/powershell/7/pwsh
     
     # 创建指向 pwsh 的符号链接
-    sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+    ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
     
     echo "PowerShell 安装完成"
 else
