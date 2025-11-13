f5c035becbf23cd5e34e22152695cfb03914c9bf
sdbds <865105819@qq.com>
2025-11-13 15:07:34 +0800

    change varity

--- 3.6、qwen_image_train_db.ps1
+++ 3.6、qwen_image_train_db.ps1
@@ -255,7 +255,7 @@ elseif (Test-Path "./.venv/bin/activate") {
 }
 
 $Env:HF_HOME = "huggingface"
-$Env:HF_ENDPOINT = "https://hf-mirror.com"
+#$Env:HF_ENDPOINT = "https://hf-mirror.com"
 $Env:XFORMERS_FORCE_DISABLE_TRITON = "1"
 $ext_args = [System.Collections.ArrayList]::new()
 $launch_args = [System.Collections.ArrayList]::new()
